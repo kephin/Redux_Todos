8d89e639df405b0d81d45970b327ca1658f1668e
kephin <wunshon76325@gmail.com>
2018-07-24 13:43:57 +0800

    chore: add redux devtool

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import registerServiceWorker from 'registerServiceWorker';
 import App from 'components/App';
 import reducers from 'reducers';
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware()));
 
 ReactDOM.render(
   (<Provider store={store}>
